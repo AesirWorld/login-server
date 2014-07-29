2c89936422430eaf34a45d6b57701f6d5d964e4d
herenow <leonardoshiro@gmail.com>
2014-07-29 20:11:34 -0300

    code update

---
+++ account/account.go
@@ -0,0 +1,60 @@
+// This packages provides you with helper functions to authenticate and manage accounts
+// It works directly with the backend database (MySQL)
+package account
+
+import "database/sql"
+import _ "github.com/ziutek/mymysql/godrv" // Go driver for database/sql package
+import "log"
+
+var db, _ = sql.Open("mymysql", "tcp:127.0.0.1:3306*ragnarok/ragnarok/ragnarok")
+
+func Connect() {
+	err := db.Ping()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Authenticate an user account, normally a player account
+func Auth(username string, password string) (account_id int) {
+	var query = "SELECT account_id FROM login WHERE sex != 'S' AND userid = ? AND user_pass = ? LIMIT 1"
+	log.Println(username, password)
+	rows, err := db.Query(query, username, password)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for rows.Next() {
+		if err := rows.Scan(&account_id); err != nil {
+			log.Fatal(err)
+		}
+
+		break
+	}
+
+	return
+}
+
+// Authenticate an server, type S on the database
+// This functions is generally used to authenticate a char-server or map-server
+func AuthServer(username string, password string) (account_id int) {
+	var query = "SELECT account_id FROM login WHERE sex = 'S' AND account_id < 2000000 AND userid = ? AND user_pass = ? LIMIT 1"
+	log.Println(username, password)
+	rows, err := db.Query(query, username, password)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for rows.Next() {
+		if err := rows.Scan(&account_id); err != nil {
+			log.Fatal(err)
+		}
+
+		break
+	}
+
+	return
+}
--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import "log"
 import "net"
 import "os"
+import "encoding/binary"
+import "strings"
+import "github.com/AesirWorld/global-server/account"
 
 type Client struct {
 	conn net.Conn
@@ -15,6 +18,9 @@ func main() {
 		log.Println(err)
 		os.Exit(1)
 	}
+
+	// Connect to account database
+	account.Connect()
 
 	// Accept connections
 	for {
@@ -42,10 +48,12 @@ func initConnection(c net.Conn) {
 		// Receive packets
 		for {
 			packet := make([]byte, 1024)
-			_, err := c.Read(packet)
 
-			if err != nil {
-				log.Println(err)
+			length, _ := c.Read(packet)
+
+			// Connection-closed
+			if length == 0 {
+				log.Println("Len 0, conenction closed")
 				break
 			}
 
@@ -59,16 +67,148 @@ func initConnection(c net.Conn) {
 			// Heartbeet packet
 			case 0x200:
 				log.Println("Heartbeet request")
+				c.Write([]byte("pong"))
 				break
 
 			// Login packets
 			case
 				0x64,   // Request client login
 				0x01dd: // Request client login with encrypt
-				userid := string(packet[6 : 6+24])   // Offset 6, length 24
-				passwd := string(packet[30 : 30+24]) // Offset 30, length 24
-				log.Println("Login request from:", userid, passwd)
+				userid := strings.TrimRight(string(packet[6:6+24]), "\x00")   // Offset 6, length 24
+				passwd := strings.TrimRight(string(packet[30:30+24]), "\x00") // Offset 30, length 24
+
+				log.Printf("Authenticating account (%s/%s) from (%s)]\n", c.RemoteAddr(), userid, passwd)
+				account_id := account.Auth(userid, passwd)
+
+				if account_id == 0 {
+					//TODO failed
+				} else {
+					//TODO sucess
+				}
+
+				return
+			//case 0x1db: next = logclif_parse_reqkey(fd, sd); break;
+			// Char-server login request
+			case 0x2710:
+				userid := strings.TrimRight(string(packet[2:2+24]), "\x00")   // Offset 6, length 24
+				passwd := strings.TrimRight(string(packet[26:26+24]), "\x00") // Offset 30, length 24
+
+				log.Printf("Authenticating char-server (%s) with (%s/%s)\n", c.RemoteAddr(), userid, passwd)
+				account_id := account.AuthServer(userid, passwd)
+
+				if account_id == 0 {
+					log.Printf("Char-server connection request rejcted (%s)\n", c.RemoteAddr())
+					id := uint16(0x2711) // packet id
+					res := make([]byte, 3)
+					binary.LittleEndian.PutUint16(res, id)
+					res[2] = 1 // Type 0 = connection accepted.
+					c.Write(res)
+				} else {
+					log.Printf("Char-server connection request accepted (%s) (%d)\n", c.RemoteAddr(), account_id)
+					id := uint16(0x2711) // packet id
+					res := make([]byte, 3)
+					binary.LittleEndian.PutUint16(res, id)
+					res[2] = 0 // Type 0 = connection accepted.
+					c.Write(res)
+					handleCharServer(c)
+				}
+				return
+			default:
+				log.Printf("Abnormal end of connection (ip: %s): Unknown packet 0x%x\n", c.RemoteAddr(), packet_id)
+				return
 			}
 		}
 	}()
+}
+
+func handleCharServer(c net.Conn) {
+	// Receive packets
+	for {
+		packet := make([]byte, 1024)
+
+		length, _ := c.Read(packet)
+
+		// Connection-closed
+		if length == 0 {
+			log.Println("Len 0, conenction closed")
+			break
+		}
+
+		// First 2 bytes represent the packet_id
+		packet_id := int(packet[0])<<0 | int(packet[1])<<8
+
+		log.Println("Received packet from char-server", packet_id)
+
+		switch packet_id {
+		/*
+			case 0x2712:
+				next = logchrif_parse_reqauth(fd, cid, ip)
+				break
+			case 0x2714:
+				next = logchrif_parse_ackusercount(fd, cid)
+				break
+			case 0x2715:
+				next = logchrif_parse_updmail(fd, cid, ip)
+				break
+			case 0x2716:
+				next = logchrif_parse_reqaccdata(fd, cid, ip)
+				break
+			case 0x2719:
+				next = logchrif_parse_keepalive(fd)
+				break
+			case 0x2722:
+				next = logchrif_parse_reqchangemail(fd, cid, ip)
+				break
+			case 0x2724:
+				next = logchrif_parse_requpdaccstate(fd, cid, ip)
+				break
+			case 0x2725:
+				next = logchrif_parse_reqbanacc(fd, cid, ip)
+				break
+			case 0x2727:
+				next = logchrif_parse_reqchgsex(fd, cid, ip)
+				break
+			case 0x2728:
+				next = logchrif_parse_updreg2(fd, cid, ip)
+				break
+			case 0x272a:
+				next = logchrif_parse_requnbanacc(fd, cid, ip)
+				break
+			case 0x272b:
+				next = logchrif_parse_setacconline(fd, cid)
+				break
+			case 0x272c:
+				next = logchrif_parse_setaccoffline(fd)
+				break
+			// User online count in this char-server
+			case 0x272d:
+				next = logchrif_parse_updonlinedb(fd, cid)
+				break
+			case 0x272e:
+				next = logchrif_parse_reqacc2reg(fd)
+				break
+			case 0x2736:
+				next = logchrif_parse_updcharip(fd, cid)
+				break
+			case 0x2737:
+				next = logchrif_parse_setalloffline(fd, cid)
+				break
+			case 0x2738:
+				next = logchrif_parse_updpincode(fd)
+				break
+			case 0x2739:
+				next = logchrif_parse_pincode_authfail(fd)
+				break
+			case 0x2740:
+				next = logchrif_parse_bankvault(fd, cid, ip)
+				break
+			case 0x2742:
+				next = logchrif_parse_reqvipdata(fd)
+				break //Vip sys`
+		*/
+		default:
+			log.Printf("Abnormal packet received from authenticated char-server (%s): %#04x\n", c.RemoteAddr(), packet_id)
+			log.Println(packet)
+		}
+	}
 }
