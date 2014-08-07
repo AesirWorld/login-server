db24becee50066a2e1aaf787ac51f8d422abfe90
herenow <leonardoshiro@gmail.com>
2014-08-07 14:33:23 -0300

    added write funcs

--- packet/parser.go
+++ packet/parser.go
@@ -11,6 +11,7 @@ type Packet struct {
 	buffer []byte
 }
 
+// Instanciates a new buffer and its reader funcs
 func Reader(buffer []byte) *Packet {
 	return &Packet{buffer}
 }
@@ -41,4 +42,37 @@ func (p *Packet) ReadInt32(offset int16) int32 {
 
 func (p *Packet) ReadString(offset int16, length int16) string {
 	return strings.TrimRight(string(p.buffer[offset:offset+length]), "\x00")
+}
+
+// Instanciates a new buffer and its writer funcs
+func Writer(buffer []byte) *Packet {
+	return &Packet{buffer}
+}
+
+func (p *Packet) WriteUint8(offset int16, payload uint8) {
+	p.buffer[offset] = payload
+}
+
+func (p *Packet) WriteInt8(offset int16, payload int8) {
+	p.buffer[offset] = uint8(payload)
+}
+
+func (p *Packet) WriteUint16(offset int16, payload uint16) {
+	binary.LittleEndian.PutUint16(p.buffer[offset:], payload)
+}
+
+func (p *Packet) WriteInt16(offset int16, payload int16) {
+	binary.LittleEndian.PutUint16(p.buffer[offset:], uint16(payload))
+}
+
+func (p *Packet) WriteUint32(offset int16, payload uint32) {
+	binary.LittleEndian.PutUint32(p.buffer[offset:], payload)
+}
+
+func (p *Packet) WriteInt32(offset int16, payload int32) {
+	binary.LittleEndian.PutUint32(p.buffer[offset:], uint32(payload))
+}
+
+func (p *Packet) WriteString(offset int16, payload string, length int16) {
+	copy(p.buffer[offset:offset+length], payload)
 }
